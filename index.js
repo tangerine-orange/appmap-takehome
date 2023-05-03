e8d564941971b0b88b02c45e5ca4764ab52a83ef
Jonathan Reis <johnny.michael.reis@gmail.com>
2023-05-03 15:53:41 -0400

    replace sqlite package with better-sqlite3, which is synchronous, and will make it easier to set up tests

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 const app = require('express')();
-const sqlite3 = require('sqlite3');
-const open = require('sqlite').open;
+const sqlite = require('better-sqlite3');
 const generateShortUrlPath = require('./generateShortUrlPath');
 
 const PORT = 8080;
 
-open({ filename: './url_shortener.db', driver: sqlite3.Database }).then((db) => {
-    db.run('CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY AUTOINCREMENT, original TEXT, short TEXT)');
+const db = sqlite(process.env.DB_PATH || './url_shortener.db', { verbose: console.log });
+db.pragma('journal_mode = WAL');
+db.exec('CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY AUTOINCREMENT, original TEXT, shortened TEXT)');
 
-
-    app.get('/urls', async (req, res) => {
-        const urls = await db.all('SELECT * FROM urls');
-        res.json(urls);
-    });
-    
-    // :url is expected to be UTF-8 encoded
-    app.post('/url/:url', async (req, res) => {
-        try {
-            const { url } = req.params;
-            const shortUrlPath = generateShortUrlPath();
-            await db.run('INSERT INTO urls (original, shortened) VALUES (?, ?)', [url, shortUrlPath]);
-            res.json({ shortUrlPath });
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server error');
-        }
-    });
-
-    app.get('/urls/:id', async (req, res) => {
-        const { id } = req.params;
-        const url = await db.get('SELECT * FROM urls WHERE id = ?', id);
-        if (url) {
-          res.json(url);
-        } else {
-          res.status(404).send('URL not found');
-        }
-    });
-
-    app.listen(
-        PORT,
-        () => console.log(`Running at http://localhost:${PORT}`)
-    );
+app.get('/urls', async (req, res) => {
+    const urls = db.prepare('SELECT * FROM urls').all();
+    res.json(urls);
 });
+
+// :url is expected to be UTF-8 encoded
+app.post('/url/:url', async (req, res) => {
+    try {
+        const { url } = req.params;
+        const shortUrlPath = generateShortUrlPath();
+        db.prepare('INSERT INTO urls (original, shortened) VALUES (?, ?)').run(url, shortUrlPath);
+        res.json({ shortUrlPath });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+app.get('/urls/:id', async (req, res) => {
+    const { id } = req.params;
+    const url = db.prepare('SELECT * FROM urls WHERE id = ?').get(id);
+    if (url) {
+        res.json(url);
+    } else {
+        res.status(404).send('URL not found');
+    }
+});
+
+app.listen(
+    PORT,
+    () => console.log(`Running at http://localhost:${PORT}`)
+);
