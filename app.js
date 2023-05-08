82cd9502ccc40fae2636292636f753f68025c77f
Jonathan Reis <johnny.michael.reis@gmail.com>
2023-05-08 14:17:02 -0400

    use https.createServer if running locally, use app.listen if in production because Heroku handles https automatically. Change client proxy from local to heroku

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const app = require('express')();
 const fs = require('fs');
 const initializeDb = require('./scripts/initializeDb.js');
 var bodyParser = require('body-parser')
+require('dotenv').config();
 
 initializeDb();
 
 let PORT;
 if (process.env.NODE_ENV === 'test') {
-    PORT = 8081;
+    PORT = process.env.TEST_PORT;
 } else {
-    PORT = 8080;
+    PORT = process.env.PORT;
 }
 
 // parse application/x-www-form-urlencoded
@@ -20,13 +21,18 @@ app.use(bodyParser.json())
 const routing = require('./routes/index.js')
 app.use('/', routing)
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("./.cert/key.pem"),
-      cert: fs.readFileSync("./.cert/cert.pem"),
-    },
-    app
-).listen(PORT, () => console.log(`Running at https://localhost:${PORT}`));
+if (process.env.NODE_ENV === 'production') {
+    app.listen(PORT, () => console.log(`Running at http://localhost:${PORT}`));
+} else {
+    https
+    .createServer(
+        {
+        key: fs.readFileSync("./.cert/key.pem"),
+        cert: fs.readFileSync("./.cert/cert.pem"),
+        },
+        app
+    ).listen(PORT, () => console.log(`Running at https://localhost:${PORT}`));
+}
+
 
 module.exports = app;
